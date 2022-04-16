a5baa8568414d2b182c1d62ac107a6e9fe1a87ec
Rhaqim <anusiemj@gmail.com>
2022-04-16 20:10:30 +0100

    Added bioSection and interests

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
-import { Container, Box, Heading, Image, useColorModeValue, Link } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+  Link,
+  Button
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import NextLink from 'next/link'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
     <Container>
       <Box
         borderRadius="lg"
-        bg={useColorModeValue("#75339c", "whiteAlpha.200")}
+        bg={useColorModeValue('#75339c', 'whiteAlpha.200')}
         p={3}
         align="center"
         color="white"
@@ -45,7 +56,50 @@ const Page = () => {
           Work
         </Heading>
         <Paragraph>
-          I am a Django Backend Developer transitioning into a fullstack developer. I currently work at <Link href='https://azenia.com'>Azenia</Link> as a backend developer.
+          I am a Django Backend Developer transitioning into a fullstack
+          developer. I currently work at{' '}
+          <Link href="https://azenia.com">Azenia</Link> on contract to
+          _.
+        </Paragraph>
+        <Box align={'center'} my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="purple">
+              See My Works
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1995</BioYear>
+          Born in Lagos, Nigeria.
+        </BioSection>
+        <BioSection>
+          <BioYear>2019</BioYear>
+          Graduated from the University of Benin with a Bachelor&apos;s in Computer Science.
+        </BioSection>
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Started Learning Python at <Link href='https://univelcity.com'>UnivelCity</Link>.
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Joined Azenia as a Django Backend Developer.
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Joined Turing as a Backend Developer.
+        </BioSection>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          I ❤️
+        </Heading>
+        <Paragraph>
+          Music {' '}, Art {' '}, and Technology. I also ejoy playing video games and learning new things. I am an AI enthusiast and a fan of the {'Tensorflow'} framework.
         </Paragraph>
       </Section>
     </Container>
