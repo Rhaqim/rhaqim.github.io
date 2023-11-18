1c742ac998abfd6b17285815175ea4489b4ce1d2
Rhaqim <anusiemj@gmail.com>
2023-11-18 03:45:48 +0100

    handling the on click

--- src/components/NavBar/LinkButton.tsx
+++ src/components/NavBar/LinkButton.tsx
@@ -5,11 +5,19 @@ type LinkButtonProps = {
   bg?: string
   color?: string
   p?: number
+  onClick?: () => void
 }
 
-const LinkButton: React.FC<LinkButtonProps> = ({ children, bg, color, p }) => {
+const LinkButton: React.FC<LinkButtonProps> = ({
+  children,
+  bg,
+  color,
+  p,
+  onClick
+}) => {
   return (
     <button
+      onClick={onClick}
       className={`${
         bg ? `bg-${bg}` : 'bg-transparent'
       } text-${color} font-semibold hover:text-#202023 py-${p} px-4 border border-transparent rounded`}
